179f01939b1219753a0eb4dea09491c124cc75b9
Daniel Da Silva Carvalho <danda-si@c3r1p5.42sp.org.br>
2026-06-03 17:12:41 -0300

    feat: implement stack_last function

--- src/Core-Engine/stack/stack_utils.c
+++ src/Core-Engine/stack/stack_utils.c
@@ -6,11 +6,25 @@
 /*   By: danda-si <danda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 12:39:38 by danda-si          #+#    #+#             */
-/*   Updated: 2026/06/03 13:03:37 by danda-si         ###   ########.fr       */
+/*   Updated: 2026/06/03 17:03:46 by danda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*stack_last(t_stack *stack);
+// retorna o último elemento da stack
+t_stack	*stack_last(t_stack *stack)
+{
+	t_stack	*current;
+
+	if (stack == NULL)
+		return (NULL);
+	current = stack;
+	while (current->next)
+	{
+		current = current->next;
+	}
+	return (current);
+}
+
 int		stack_size(t_stack *stack);
