b825ee8dbbd70458a7245c36322181a80e26b1bf
Daniel Da Silva Carvalho <danda-si@c3r1p5.42sp.org.br>
2026-06-08 13:19:30 -0300

    feat: implement ft_stack_is_sorted function

--- src/Core-Engine/stack/stack_check.c
+++ src/Core-Engine/stack/stack_check.c
@@ -6,10 +6,26 @@
 /*   By: danda-si <danda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 12:39:05 by danda-si          #+#    #+#             */
-/*   Updated: 2026/06/04 14:33:24 by danda-si         ###   ########.fr       */
+/*   Updated: 2026/06/08 13:18:45 by danda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_stack_is_sorted(t_stack *stack);
+//Verifica se a pilha está ordenada em ordem crescente
+//(retorna 1 se estiver ordenada, 0 caso contrário).
+int	ft_stack_is_sorted(t_stack *stack)
+{
+	t_stack	*current;
+
+	if (stack == NULL || stack->next == NULL)
+		return (1);
+	current = stack;
+	while (current->next)
+	{
+		if (current->value > current->next->value)
+			return (0);
+		current = current->next;
+	}
+	return (1);
+}
