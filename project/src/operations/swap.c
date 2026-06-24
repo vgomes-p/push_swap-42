9f7a0946111045dbe6cb3fc3617067182e2be1ef
Daniel Da Silva Carvalho <danda-si@c2r11p4.42sp.org.br>
2026-06-24 16:24:48 -0300

    feat: implement op_sa operation

--- project/src/operations/swap.c
+++ project/src/operations/swap.c
@@ -6,14 +6,29 @@
 /*   By: danda-si <danda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 18:15:32 by danda-si          #+#    #+#             */
-/*   Updated: 2026/06/23 18:15:51 by danda-si         ###   ########.fr       */
+/*   Updated: 2026/06/24 16:24:03 by danda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
 
+//Trocar os dois primeiros nós da stack A.
 void	op_sa(t_stack **a)
 {
+	t_stack	*first;
+	t_stack	*second;
+
+	if (a == NULL || *a == NULL || (*a)->next == NULL)
+		return ;
+	first = *a;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	if (first->next != NULL)
+		first->next->prev = first;
+	first->prev = second;
+	second->prev = NULL;
+	*a = second;
 }
 
 void	op_sb(t_stack **b)
