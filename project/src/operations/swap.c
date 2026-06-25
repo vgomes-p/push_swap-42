ac47322cf1671d1dcb05176583148ad3e50d7aff
Daniel Da Silva Carvalho <danda-si@c3r2p1.42sp.org.br>
2026-06-25 14:30:24 -0300

    refactor: reuse swap logic for swap operations

--- project/src/operations/swap.c
+++ project/src/operations/swap.c
@@ -6,38 +6,43 @@
 /*   By: danda-si <danda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 18:15:32 by danda-si          #+#    #+#             */
-/*   Updated: 2026/06/24 16:36:56 by danda-si         ###   ########.fr       */
+/*   Updated: 2026/06/25 14:27:27 by danda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
 
 //Trocar os dois primeiros nós da stack A.
-void	op_sa(t_stack **a)
+static	void	op_swap(t_stack **stack)
 {
 	t_stack	*first;
 	t_stack	*second;
 
-	if (a == NULL || *a == NULL || (*a)->next == NULL)
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 		return ;
-	first = *a;
+	first = *stack;
 	second = first->next;
 	first->next = second->next;
 	second->next = first;
-	if (first->next != NULL)
-		first->next->prev = first;
+	if (first != NULL)
+	first->next->prev = first;
 	first->prev = second;
 	second->prev = NULL;
-	*a = second;
+	*stack = second;
+}
+
+void	op_sa(t_stack **a)
+{
+	op_swap(a);
 }
 
 void	op_sb(t_stack **b)
 {
-	(void)b;
+	op_swap(b);
 }
 
 void	op_ss(t_stack **a, t_stack **b)
 {
-	(void)a;
-	(void)b;
+	op_swap(a);
+	op_swap(b);
 }
