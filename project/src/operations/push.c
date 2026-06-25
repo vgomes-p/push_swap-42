511a593fc3eab7c30179672351b0598bf6d458c1
Daniel Da Silva Carvalho <danda-si@c3r2p1.42sp.org.br>
2026-06-25 16:17:40 -0300

    feat: implement push operations

--- project/src/operations/push.c
+++ project/src/operations/push.c
@@ -6,16 +6,35 @@
 /*   By: danda-si <danda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 18:15:59 by danda-si          #+#    #+#             */
-/*   Updated: 2026/06/23 18:16:10 by danda-si         ###   ########.fr       */
+/*   Updated: 2026/06/25 16:15:38 by danda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
 
+static void	op_push(t_stack **src, t_stack **dst)
+{
+	t_stack	*node;
+
+	if (src == NULL || *src == NULL || dst == NULL)
+		return ;
+	node = *src;
+	*src = node->next;
+	if (*src != NULL)
+		(*src)->prev = NULL;
+	node->next = *dst;
+	if (*dst != NULL)
+		(*dst)->prev = node;
+	node->prev = NULL;
+	*dst = node;
+}
+
 void	op_pa(t_stack **a, t_stack **b)
 {
+	op_push(b, a);
 }
 
 void	op_pb(t_stack **a, t_stack **b)
 {
+	op_push(a, b);
 }
