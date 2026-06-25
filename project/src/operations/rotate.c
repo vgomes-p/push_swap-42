cbf040ff313b88803087b298d0b3957cb59a0d2e
Daniel Da Silva Carvalho <danda-si@c3r1p5.42sp.org.br>
2026-06-25 18:49:34 -0300

    feat: implement rotate operations

--- project/src/operations/rotate.c
+++ project/src/operations/rotate.c
@@ -6,20 +6,40 @@
 /*   By: danda-si <danda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 18:16:18 by danda-si          #+#    #+#             */
-/*   Updated: 2026/06/23 18:16:22 by danda-si         ###   ########.fr       */
+/*   Updated: 2026/06/25 18:49:12 by danda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
 
+static void	op_rotate(t_stack **stack)
+{
+	t_stack	*first;
+	t_stack	*last;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return ;
+	first = *stack;
+	last = stack_last(*stack);
+	*stack = first->next;
+	(*stack)->prev = NULL;
+	last->next = first;
+	first->prev = last;
+	first->next = NULL;
+}
+
 void	op_ra(t_stack **a)
 {
+	op_rotate(a);
 }
 
 void	op_rb(t_stack **b)
 {
+	op_rotate(b);
 }
 
 void	op_rr(t_stack **a, t_stack **b)
 {
+	op_rotate(a);
+	op_rotate(b);
 }
