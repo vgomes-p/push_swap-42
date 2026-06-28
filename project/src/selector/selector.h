4c3357616d245a39a0f1e23c6fdc27f39a15fa89
vgomes-p <vigomes-@student.42sp.org.br>
2026-06-28 15:10:47 -0300

    perf(t_selector): adding bench to struct

--- project/src/selector/selector.h
+++ project/src/selector/selector.h
@@ -6,23 +6,36 @@
 /*   By: vigomes- <vigomes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 14:15:41 by vigomes-          #+#    #+#             */
-/*   Updated: 2026/06/15 16:38:02 by vigomes-         ###   ########.fr       */
+/*   Updated: 2026/06/28 15:09:30 by vigomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SELECTOR_H
 # define SELECTOR_H
 
-# include "../../includes/push_swap.h"
+# include "../stack/stack.h"
 
-typedef struct	s_selector
+typedef struct s_stack t_stack;
+
+# include "../index/index.h"
+
+void	index_set(t_stack *stack);
+
+typedef struct s_parser t_parser;
+
+typedef struct s_selector
 {
 	double	disorder;
 	char	*strategy;
 	int		id;
+	int		n_ops;
+	int		bench;
 }				t_selector;
 
 double	ds_local_calculator(t_stack *stack);
 double	ds_global_calculator(t_stack *stack);
+
+void		slc_adaptive(t_selector *slc, double disorder);
+t_selector	*selector(t_stack *stack, t_parser	*parser);
 
 #endif
