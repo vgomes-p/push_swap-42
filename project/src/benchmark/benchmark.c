c25b9a3d3b479667eabd5b11cf27f27b60cc7e8f
Daniel Da Silva Carvalho <danda-si@c2r11p1.42sp.org.br>
2026-06-28 18:57:44 -0300

    feat: add benchmark module foundation

--- project/src/benchmark/benchmark.c
+++ project/src/benchmark/benchmark.c
@@ -6,7 +6,7 @@
 /*   By: danda-si <danda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/27 18:07:11 by danda-si          #+#    #+#             */
-/*   Updated: 2026/06/28 16:24:14 by danda-si         ###   ########.fr       */
+/*   Updated: 2026/06/28 16:54:03 by danda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,62 @@
 void	benchmark_init(t_count *count)
 {
 	if (count == NULL)
+		return ;
+	ft_bzero(count, sizeof(t_count));
+}
+
+static int	benchmark_count_swap(t_count *count, char *operation)
+{
+	if (ft_strncmp(operation, "sa", 3) == 0)
+		return (count->sa++, count->total++, 1);
+	if (ft_strncmp(operation, "sb", 3) == 0)
+		return (count->sb++, count->total++, 1);
+	if (ft_strncmp(operation, "ss", 3) == 0)
+		return (count->ss++, count->total++, 1);
+	return (0);
+}
+
+static int	benchmark_count_push(t_count *count, char *operation)
+{
+	if (ft_strncmp(operation, "pa", 3) == 0)
+		return (count->pa++, count->total++, 1);
+	if (ft_strncmp(operation, "pb", 3) == 0)
+		return (count->pb++, count->total++, 1);
+	return (0);
+}
+
+static int	benchmark_count_rotate(t_count *count, char *operation)
+{
+	if (ft_strncmp(operation, "ra", 3) == 0)
+		return (count->ra++, count->total++, 1);
+	if (ft_strncmp(operation, "rb", 3) == 0)
+		return (count->rb++, count->total++, 1);
+	if (ft_strncmp(operation, "rr", 3) == 0)
+		return (count->rr++, count->total++, 1);
+	return (0);
+}
+
+static int	benchmark_count_reverse(t_count *count, char *operation)
+{
+	if (ft_strncmp(operation, "rra", 4) == 0)
+		return (count->rra++, count->total++, 1);
+	if (ft_strncmp(operation, "rrb", 4) == 0)
+		return (count->rrb++, count->total++, 1);
+	if (ft_strncmp(operation, "rrr", 4) == 0)
+		return (count->rrr++, count->total++, 1);
+	return (0);
+}
+
+void	benchmark_count(t_count *count, char *operation)
+{
+	if (count == NULL || operation == NULL)
+		return ;
+	if (benchmark_count_swap(count, operation) == 1)
+		return ;
+	if (benchmark_count_push(count, operation) == 1)
+		return ;
+	if (benchmark_count_rotate(count, operation) == 1)
+		return ;
+	if (benchmark_count_reverse(count, operation) == 1)
 		return ;
 }
